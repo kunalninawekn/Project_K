f82a1897f7bb7fbfbcbb3e35fc1e17d5f7fa05f4
HP <HP@HPP>
2022-10-19 16:13:09 +0530

    Git ready

---
+++ src/git_one/Demo_one.java
@@ -0,0 +1,11 @@
+package git_one;
+
+public class Demo_one {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("first code for git");
+		
+	}
+
+}
